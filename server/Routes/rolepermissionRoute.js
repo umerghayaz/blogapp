020d5a3ff0f0b8843ad3a23c3eb1bd2c04aa2635
umerghayaz <umershahid667@gmail.com>
2025-04-06 15:56:58 +0500

    updated servver

--- server/Routes/rolepermissionRoute.js
+++
@@ -1,7 +0,0 @@
-import { getRolePermission, deleteRolePermission, createRolePermission } from "../Controllers/rolepermissionController";
-import express from "express";
-const router = express.Router();
-router.get("/getRolePermission", getRolePermission);
-router.delete("/deleteRolePermission", deleteRolePermission);
-router.post("/createRolePermission", createRolePermission);
-export default router;
